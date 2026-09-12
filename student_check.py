e7284294de78c0fab11b7f4540a38ee26fed5902
zhoutingyu1116-blip <zhoutingyu1116@gmail.com>
2026-09-12 19:48:33 +0800

    Add logic operate practice

---
+++ student_check.py
@@ -0,0 +1,16 @@
+age = input ("How old are you?")
+age = int (age)
+score = input ("What is your score?")
+score = int (score)
+has_permission = False
+
+if not has_permission:
+    print ("You do not have permission.")
+elif age < 10 or age >70:
+    print ("You are not allowed.")
+elif age < 18:
+    print ("You are too young.")
+elif score < 60:
+    print ("Your score is too low.")
+else:
+    print ("You are eligible.")
